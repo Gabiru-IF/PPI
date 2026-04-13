fdc52445d5dc23f5353aad91d3d4c3df5a122c71
Gabiru-IF <gabriel.moreira@escolar.ifrn.edu.br>
2026-04-13 11:55:28 -0300

    push

---
+++ POO/poo1.ts
@@ -0,0 +1,25 @@
+let data = new Date()
+
+console.log(data.getDay())
+
+class Data {
+    dia : number;
+    mes : number;
+    ano : number
+
+    constructor(dd : number = 1, mm : number = 1, aa : number = 2026) {
+        this.dia = dd
+        this.mes = mm
+        this.ano = aa
+    }
+
+    obterDataFormatada() : string {
+        return `${this.dia}/${this.mes}/${this.ano}`
+    }
+}
+
+let hoje = new Data(13, 4, 2026)
+console.log(hoje.obterDataFormatada())
+
+let outraData = new Data(13)
+console.log(outraData.obterDataFormatada())
