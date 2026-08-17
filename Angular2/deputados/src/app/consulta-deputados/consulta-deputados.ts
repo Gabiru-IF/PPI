c9aad542d87ad52dfec34d9f19f89d4e54629a8f
Gabiru-IF <gabriel.moreira@escolar.ifrn.edu.br>
2026-08-17 07:34:00 -0300

    atividade_deputados_concluida

--- Angular2/deputados/src/app/consulta-deputados/consulta-deputados.ts
+++ Angular2/deputados/src/app/consulta-deputados/consulta-deputados.ts
@@ -1,10 +1,11 @@
 import { Component, inject, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { DeputadoService } from '../deputado-service';
 import { Deputado } from '../deputado';
 
 @Component({
   selector: 'app-consulta-deputados',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './consulta-deputados.html',
   styleUrl: './consulta-deputados.scss',
 })
@@ -13,23 +14,38 @@ export class ConsultaDeputados {
   readonly #deputadoService = inject(DeputadoService);
   protected deputados = signal<Deputado[]>([]);
 
+  protected readonly filtroForm = new FormGroup({
+    nome: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.minLength(2)],
+    }),
+  });
+
   constructor() {
-  
     this.#deputadoService.obterTodos().subscribe(res => {
       this.deputados.set(res.dados);
-    
     });
   }
 
-  filtrarDeputados(nome: string) {
-    if (nome.trim() === '') {
+  filtrarDeputados() {
+    const nome = this.filtroForm.get('nome')?.value?.trim() ?? '';
+
+    if (nome === '') {
       this.#deputadoService.obterTodos().subscribe(res => {
         this.deputados.set(res.dados);
       });
-    } else {
-      this.#deputadoService.obterDeputadoPorNome(nome).subscribe(res => {
-        this.deputados.set(res.dados);
-      });
+      this.filtroForm.markAsUntouched();
+      return;
     }
+
+    if (this.filtroForm.invalid) {
+      alert('Digite pelo menos 2 caracteres para buscar.');
+      this.filtroForm.markAllAsTouched();
+      return;
+    }
+
+    this.#deputadoService.obterDeputadoPorNome(nome).subscribe(res => {
+      this.deputados.set(res.dados);
+    });
   }
 }
